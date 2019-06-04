109074ea7cae0d5ad1a8af63b5199561bda770e6
William Granados <william.granadosmatamoros@mail.utoronto.ca>
2019-06-04 01:24:34 -0400

    added logic for generating panels

--- src/panel-rooms.tsx
+++ src/panel-rooms.tsx
@@ -11,11 +11,47 @@ class RoomsRoom extends PSRoom {
 
 class RoomsPanel extends PSRoomPanel {
 	render() {
+		const roomInfo = (link: string, numUsers: number, name: string, desc: string) => {
+			return <div>
+				<a 
+					href={link}
+					class="ilink">
+						<small 
+							style="float:right">({numUsers} users)
+						</small>
+						<strong>
+							<i class="fa fa-comment-o"></i> {name} <br></br>
+						</strong>
+						<small> {desc} </small>
+					</a>
+				</div>
+		};
+		const rooms = [{link: '/lobby', users: 10, name: 'Lobby', desc: 'Welcome to tours!'}, 
+									 {link: '/tours', users: 20, name: 'Tours', desc: 'Welcome to tours!'} ];
+		const roomList = rooms.map(room => roomInfo(room.link, room.users, room.name, room.desc) );
 		return <PSPanelWrapper room={this.props.room}>
 			<div class="mainmessage">
-				<p>[insert room list here]</p>
-				<p><a href="/lobby">Lobby</a></p>
-				<p><a href="/tours">Tours</a></p>
+				<tbody>
+					<tr>
+						<td>
+							<span 
+								style="background:transparent url(https://play.pokemonshowdown.com/sprites/smicons-sheet.png?a5) no-repeat scroll -0px -2790px;" 
+								class="picon icon-left" 
+								title="Meloetta is PS's mascot! The Aria forme is about using its voice, and represents our chatrooms.">
+							</span> 
+							<button class="button" name="finduser" title="Find an online user"><strong>9920</strong> users online</button>
+						</td>
+						<td>
+							<button class="button" name="roomlist" title="Watch an active battle"><strong>1512</strong> active battles</button> 
+							<span 
+								style="background:transparent url(https://play.pokemonshowdown.com/sprites/smicons-sheet.png?a5) no-repeat scroll -0px -2220px" 
+								class="picon icon-right" 
+								title="Meloetta is PS's mascot! The Pirouette forme is Fighting-type, and represents our battles.">
+							</span>
+						</td>
+					</tr>
+				</tbody>
+				{roomList}
 			</div>
 		</PSPanelWrapper>;
 	}
